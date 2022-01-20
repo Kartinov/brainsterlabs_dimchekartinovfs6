6d1d8459047c16e7d44effeba3561fc0df091cee
Dimche Kartinov <dimchekartinov@gmail.com>
2022-01-20 19:30:57 +0100

    Added card-menu with buttons who can filter cards depending on their data-atribute, arrays of objects are added and JavaSciprt code for appending the HTML <elements> and data from the array.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,267 @@
+let projects = [
+  {
+    id: 1,
+    category: 'marketing',
+    img: 'marketing.jpg',
+    academy: 'Академија за маркетинг',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 2,
+    category: 'marketing',
+    img: 'marketing.jpg',
+    academy: 'Академија за маркетинг',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 3,
+    category: 'marketing',
+    img: 'marketing.jpg',
+    academy: 'Академија за маркетинг',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 4,
+    category: 'marketing',
+    img: 'marketing.jpg',
+    academy: 'Академија за маркетинг',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 5,
+    category: 'marketing',
+    img: 'marketing.jpg',
+    academy: 'Академија за маркетинг',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 6,
+    category: 'marketing',
+    img: 'marketing.jpg',
+    academy: 'Академија за маркетинг',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 7,
+    category: 'design',
+    img: 'design.png',
+    academy: 'Академија за дизајн',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 8,
+    category: 'design',
+    img: 'design.png',
+    academy: 'Академија за дизајн',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 9,
+    category: 'design',
+    img: 'design.png',
+    academy: 'Академија за дизајн',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 10,
+    category: 'design',
+    img: 'design.png',
+    academy: 'Академија за дизајн',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 10,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 11,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 12,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 13,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 14,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 15,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 16,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 17,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 18,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 19,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+  {
+    id: 20,
+    category: 'coding',
+    img: 'coding.jpg',
+    academy: 'Академија за програмирање',
+    title: 'Име на проектот стои овде во две линии',
+    description: 'Краток опис во кој студентите ке можат да опишат за што се работи во проектот.',
+    date: 'Април - Октомври 2019',
+  },
+];
+
+var active_tab = null;
+
 const menuBtn = document.querySelector('.menu-btn');
 const menuItems = document.querySelector('.nav-menu-items');
 const menuItem = document.querySelectorAll('.menu-item');
 
 // main toggle
 menuBtn.addEventListener('click', () => {
- toggle();
+  toggle();
 });
 
 // toggle on item click if open
 menuItem.forEach((item) => {
   item.addEventListener('click', () => {
-    if(menuBtn.classList.contains('open')) {
+    if (menuBtn.classList.contains('open')) {
       toggle();
     }
   });
 });
 
 function toggle() {
-   menuBtn.classList.toggle('open');
+  menuBtn.classList.toggle('open');
   menuItems.classList.toggle('open');
+}
+
+// FILTERING CARDS
+$('.card-menu button').click(function () {
+  active_tab = $(this).data('target') != active_tab ? $(this).data('target') : null;
+
+  $('.card-menu button').each(function (index, button) {
+    if ($(button).data('target') == active_tab) {
+      $(button).addClass('checked');
+    } else {
+      $(button).removeClass('checked');
+    }
+  });
+
+  filterProjects();
+});
+
+$(document).ready(function () {
+  filterProjects();
+});
+
+function filterProjects() {
+  let items = projects;
+
+  if (active_tab != null) {
+    items = projects.filter(function (project) {
+      return project.category == active_tab;
+    });
+  }
+
+  // Clear <element>
+  $('.cards').html('');
+
+  $.each(items, function (index, project) {
+    $('.cards').append(
+      `
+        <div href="#" class="card">
+          <div class="card-inner">
+            <img src="./assets/img/${project.img}" alt="">
+            <div class="card-info">
+              <span class="academy">${project.academy}</span>
+              <h3 class="title">${project.title}</h3>
+              <p>${project.description}</p>
+              <p class="date">${project.date}</p>
+              <a href="/projects/${project.id}" class="btn">Дознај повеќе</a>
+           </div>
+         </div>
+       </div>
+  `
+    );
+  });
 }
