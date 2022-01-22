55369eaa860de7f1e05c5270a323b7e48e5bd0e8
Dimche Kartinov <dimchekartinov@gmail.com>
2022-01-22 11:46:19 +0100

    Added footer & header into includes, and include them into index.php

---
+++ assets/includes/footer.php
@@ -0,0 +1,9 @@
+  <footer>
+    <p>Изработено со <i class="fas fa-heart"></i> од студентите на Brainster</p>
+  </footer>
+
+  <script src="./assets/js/jquery.min.js"></script>
+  <script src="./assets/js/main.js"></script>
+  </body>
+
+  </html>
---
+++ form.php
@@ -0,0 +1,7 @@
+<?php 
+$title = "Student Request";
+require_once './assets/includes/header.php'; ?>
+
+<h1>HELOO</h1>
+
+<?php include_once './assets/includes/footer.php';
